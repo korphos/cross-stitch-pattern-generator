5461c77d262b61e3a5288298e3dbe93b6b7a67c6
Alexandre Chenieux <alexandre.chenieux@gmail.com>
2026-07-03 22:13:39 -0400

    feat: add need to buy stat

--- src/components/PalettePanel.tsx
+++ src/components/PalettePanel.tsx
@@ -120,7 +120,7 @@ export function PalettePanel({ project, dispatch, sizeUnit }: Props) {
             <dt className="text-neutral-500">Thread needed</dt>
             <dd>{totalSkeins} skeins</dd>
           </div>
-          {project.paletteMode === 'ownedOnly' && hasInventory && (
+          {hasInventory && (
             <div className="flex justify-between gap-2">
               <dt className="text-neutral-500">Need to buy</dt>
               <dd>{notOwnedCount} colors</dd>
